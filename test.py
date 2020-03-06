62441f236d21b26cf592044bb5da4c5e8a10fb7d
jaswanth098 <jaswanth@deepcompute.com>
2020-03-06 18:39:19 +0530

    adding test file

---
+++ test.py
@@ -0,0 +1,38 @@
+from graphqlclient import GraphQLClient
+import asyncio
+
+gc = GraphQLClient("ws://domain:<port>/graphql")
+query = """
+{
+  students{
+    id
+    name
+    age
+
+  }
+}
+"""
+# resp = gc.execute(query)
+# print(resp)
+
+sub= """
+subscription {
+  teacher{
+    mutation
+    updatedFields
+    node{
+      id
+      name
+      age
+    }
+  }
+}
+"""
+
+async def main():
+    s = gc.subscription(sub)
+    await s.start()
+    async for i in s.listen():
+        print(i)
+
+asyncio.get_event_loop().run_until_complete(main())
